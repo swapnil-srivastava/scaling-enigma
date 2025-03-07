ef95c2ce224f294a62fb5db546193bc823532761
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2025-03-07 21:19:30 +0000

    adding the component with rollup

---
+++ packages/component-1/rollup.config.js
@@ -0,0 +1,30 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
+
+export default {
+  input: 'src/Component1.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+    }),
+    terser(),
+  ],
+};
