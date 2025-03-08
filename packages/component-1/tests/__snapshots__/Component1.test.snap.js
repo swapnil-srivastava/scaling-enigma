955b5ac7e4ab004281a0b7d1d8eab6b2a3acf648
Swapnil Srivastava <swapnilsrivastava68@hotmail.com>
2025-03-08 21:53:02 +0000

    adding updated snapshot for the component 1

--- packages/component-1/tests/__snapshots__/Component1.test.snap.js
+++ packages/component-1/tests/__snapshots__/Component1.test.snap.js
@@ -2,10 +2,14 @@
 export const snapshots = {};
 
 snapshots["Component1 matches the snapshot"] = 
-`<input type="text">
-<p>
-  Component 1
-</p>
+`<div>
+  <input type="text">
+  <p>
+    Component 1
+  </p>
+  <div class="user-list">
+  </div>
+</div>
 `;
 /* end snapshot Component1 matches the snapshot */
 
